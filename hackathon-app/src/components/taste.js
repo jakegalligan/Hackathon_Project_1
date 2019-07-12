94084d855ff45a374c3a0bad7f10e3e12880f7e5
Jake <galligan1996@gmail.com>
2019-07-12 09:54:03 -0400

    hello

--- hackathon-app/src/components/taste.js
+++ hackathon-app/src/components/taste.js
@@ -1,25 +1,62 @@
 import React, { Component } from "react";
 
+
+{/*<p>{taste.wTeaser}</p>
+<a href={taste.wUrl}>{taste.wUrl}</a> */}
+
+const cardClick = (e) => {
+
+}
+
+// const categoryIcon = (category) => {
+//   var upperCaseCat = category.toUpperCase();
+//   console.log(upperCaseCat);
+//   switch(upperCaseCat) {
+//     case "MUSIC":
+//       return <i className="fas fa-music fa-fw"></i>;
+//     case "MOVIE":
+//       return <i className="fas fa-film fa-fw"></i>;
+//     case "SHOW":
+//       return <i className="fas fa-tv fa-fw"></i>;
+//     case "BOOK":
+//       return <i className="fas fa-book fa-fw"></i>;
+//     case "AUTHOR":
+//       return <i className="fas fa-male fa-fw"></i>;
+//     case "GAME":
+//       return <i className="fas fa-gamepad fa-fw"></i>;
+//     default:
+//       return <b>No Category</b>;
+//   }
+// }
+
+// onClick={this.changeClick}
+
 class Taste extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props){
+    super (props);
 
     this.state = {
       showPopUp: false
     }
     this.togglePopUp = this.togglePopUp.bind(this);
+    this.load = this.load.bind(this);
   }
 
   togglePopUp() {
+    console.log(this.props)
     this.setState({
       showPopUp: !this.state.showPopUp
     })
   }
 
+  load() {
+     console.log('error');
+  }
 
-  categoryIcon(category) {
+
+  categoryIcon (category){
     var upperCaseCat = category.toUpperCase();
-    switch (upperCaseCat) {
+    switch(upperCaseCat) {
       case "MUSIC":
         return <i className="fas fa-music fa-fw"></i>;
       case "MOVIE":
@@ -38,28 +75,30 @@ class Taste extends Component {
   }
 
 
-  render() {
+  render () {
     return (
+  <div className={"card mx-1 mt-1 " + (this.props.taste.Type)} key={this.props.taste.id} onClick = {this.togglePopUp} >
+         <div className="card-body">
+           <h4 className="card-title text-center">{this.props.taste.Name}</h4>
+           <div className="card-text">
+             <div className="icon text-center">{this.categoryIcon(this.props.taste.Type)}</div>
 
-      <div className={"card mx-1 mt-1 " + (this.props.taste.Type)} key={this.props.taste.id} onClick={this.togglePopUp} >
-        <div className="card-body">
-          <h4 className="card-title text-center">{this.props.taste.Name}</h4>
-          <div className="card-text">
-            <div className="icon text-center">{this.categoryIcon(this.props.taste.Type)}</div>
-
-            {this.state.showPopUp ? <div className='popup'>
-              <div className='popup_inner'>
-                <h1 className='popupHead'>{this.props.taste.Name}</h1>
-                <p className='popupInfo'>{this.props.taste.wTeaser}</p>
-                <iframe src={this.props.taste.yUrl} width="400" height="300"></iframe>
-                <button onClick={this.togglePopUp} >close me</button>
-              </div>
-            </div> : null}
-          </div>
-        </div>
-      </div>
-    )
-  }
+             {this.state.showPopUp ?  <div className='popup'>
+                <div className='popup_inner'>
+                  <h1 className = 'text-center popupHead'>{this.props.taste.Name}</h1>
+                  <p className = 'popupInfo'>{this.props.taste.wTeaser}</p>
+                  {this.props.taste.yUrl !== null ? <iframe src={this.props.taste.yUrl} width="700" height="350" align='middle' onError={this.load}></iframe>
+                : <br /> }
+                  <div>
+                    <button className='close-button btn btn-primary' onClick={this.togglePopUp} >Close</button>
+                  </div>
+                </div>
+              </div> : null }
+           </div>
+         </div>
+       </div>
+     )
+}
 }
 
 export default Taste
